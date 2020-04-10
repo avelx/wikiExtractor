654baba93b4da6a893a96e0cf043eb9596adf0e5
avelreserch <pavel.oliynyk@gmail.com>
2020-04-10 18:24:14 +0100

    Release new version

--- build/update.pl
+++ build/update.pl
@@ -8,7 +8,7 @@ system("rm -f wikiextractor_latest.jar");
 my $api_url = "http://192.168.1.8:8081/service/rest/v1/search/assets?maven.artifactId=wikiextractor_2.12";
 my $content = get($api_url);
 
-die "Can't GET $url" if (! defined $content);
+die "Can't GET $api_url" if (! defined $content);
 
 # Process JSON result
 my $json = parse_json ($content);
