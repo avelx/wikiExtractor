77b62d7d3b32ae3d35d5f2c8c0e7df0861a76802
avelreserch <pavel.oliynyk@gmail.com>
2020-04-10 14:56:28 +0100

    Update release process;

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Spark based job to transform wikipedia xml file.
 
 Overrride config file:
   -Dconfig.file=path/to/config-file
+  
+  
+Typical way to run the latest version:
+
+  ./update-jar.sh ; ./runner.sh
+  
+Make sure you refer to the latest version in update-jar.sh script.
--- build/update-jar.sh
+++ build/update-jar.sh
@@ -1,1 +1,7 @@
-curl http://192.168.1.8:8081/repository/jerusalemh/com/avel/data/wikiextractor_2.12/0.0.14/wikiextractor_2.12-0.0.14.jar -o wikiextractor.jar
+set -e
+
+if [ -e build/wiki-extractor.jar ];  then
+  rm -f 'wiki-extractor.jar'
+fi
+
+curl http://192.168.1.8:8081/repository/jerusalemh/com/avel/data/wikiextractor_2.12/0.0.14/wikiextractor_2.12-0.0.15.jar -o wikiextractor.jar
