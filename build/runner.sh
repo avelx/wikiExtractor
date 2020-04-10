84f29c32533e351317242b1100a0f150b83aa79d
avelreserch <pavel.oliynyk@gmail.com>
2020-04-10 18:14:31 +0100

    Release new version

--- build/runner.sh
+++ build/runner.sh
@@ -1,3 +1,10 @@
+#! /bin/sh
+
+set -e
+
+# re-load artifact
+eval('perl update.pl')
+
 java -Dconfig.file=application.conf -cp "wiki-extractor.jar:libs/*" com.avel.data.WikiAgg
 
 #java -Dconfig.file=application.conf -cp wiki-extractor.jar com.avel.data.WikiExtractor
