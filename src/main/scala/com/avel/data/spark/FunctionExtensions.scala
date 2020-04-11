4e5fbe58fc50b494573ca3c64a0b74b94b4322ed
avelreserch <pavel.oliynyk@gmail.com>
2020-04-11 11:39:33 +0100

    Fix issue with persisting to text file

--- src/main/scala/com/avel/data/spark/FunctionExtensions.scala
+++ src/main/scala/com/avel/data/spark/FunctionExtensions.scala
@@ -3,7 +3,7 @@ package com.avel.data.spark
 object FunctionExtensions {
   import org.apache.spark.sql.functions._
 
-  val stringify = udf((vs: Seq[String]) => vs match {
+  val stringify = udf((vs: Seq[Long]) => vs match {
 
     case null => null
 
